022ca7968422f6cd5d237db6ff7682756b953868
Matías Leporini <matilepo03@gmail.com>
2026-03-16 18:27:12 -0300

    Added first version of iddfs

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,7 +1,9 @@
 from algorithms.dfs import dfs
 from algorithms.bfs import bfs
+from algorithms.iddfs import iddfs
 
 ALGORITHMS = {
     "dfs": dfs,
     "bfs": bfs,
+    "iddfs": iddfs,
 }
---
+++ algorithms/iddfs.py
@@ -0,0 +1,60 @@
+import time
+from utils.state import SokobanState, get_successors
+from algorithms.utils import SearchNode, SearchResult
+
+
+def depth_limited_search(
+    node: SearchNode,
+    depth_limit: int,
+    path_states: set[SokobanState],
+) -> tuple[SearchNode | None, int]:
+    if node.state.is_solved():
+        return node, 1
+
+    if depth_limit <= 0:
+        return None, 0
+
+    if node.state in path_states:
+        return None, 0
+
+    path_states.add(node.state)
+    expanded = 1
+
+    for direction, new_state in get_successors(node.state):
+        if new_state not in path_states:
+            child = SearchNode(new_state, parent=node, action=direction, cost=node.cost + 1)
+            result, child_expanded = depth_limited_search(child, depth_limit - 1, path_states)
+            expanded += child_expanded
+            if result is not None:
+                path_states.remove(node.state)
+                return result, expanded
+
+    path_states.remove(node.state)
+    return None, expanded
+
+
+def iddfs(initial_state: SokobanState) -> SearchResult:
+    start_time = time.time()
+    total_expanded = 0
+    depth = 0
+
+    while True:
+        result, expanded = depth_limited_search(
+            SearchNode(initial_state),
+            depth_limit=depth,
+            path_states=set(),
+        )
+        total_expanded += expanded
+
+        if result is not None:
+            elapsed = time.time() - start_time
+            return SearchResult(
+                success=True,
+                path=result.get_path(),
+                cost=result.cost,
+                expanded_nodes=total_expanded,
+                frontier_nodes=0,
+                processing_time=elapsed,
+            )
+
+        depth += 1
