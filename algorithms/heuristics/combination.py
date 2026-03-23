6e4d7618994a877c0dfc28b8516b8c6e72164021
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2026-03-22 21:08:43 -0300

    combination with basic hungarian

--- algorithms/heuristics/combination.py
+++ algorithms/heuristics/combination.py
@@ -1,6 +1,7 @@
+from algorithms.heuristics.basic_hungarian_plus_player_distance import basic_hungarian_plus_player_distance
 from utils.state import SokobanState
 from algorithms.heuristics.deadlock import deadlock_heuristic 
 from algorithms.heuristics.emm import emm_heuristic
 
 def combination_heuristic(state: SokobanState) -> float:
-    return max(deadlock_heuristic(state), emm_heuristic(state))
+    return max(deadlock_heuristic(state), basic_hungarian_plus_player_distance(state))
